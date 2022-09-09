bce8318fcb511776708e156b827c68ec22f7fe3b
Maria Camila Tibasosa Molina <90457433+macatimo17@users.noreply.github.com>
2022-09-09 11:45:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import json
 import urllib3
 import boto3
-#Bryan Garavito
-#Creacion de la funcion que hace el scraping de la pagina del banco de la republica
-#y lo guarda en un archivo txt llamado dolar-timestamp.txt
+
+#Creacion de la funcion lambda que hace el scraping de la pagina del banco de la republica
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
@@ -11,6 +10,7 @@ def lambda_handler(event, context):
     url = 'https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario'
 
     resp = http.request('GET', url)
+    #Crea archivo dónde almacena las horas.
     archivo = open("dolar-timestamp.txt", "w")
     archivo.write(print(resp.data.decode('utf -8')))
 
